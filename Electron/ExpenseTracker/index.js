6becc6c8508be1b18e28590298beb0be1687bed6
HugoNogueira05 <hugonogueira055@gmail.com>
2024-06-18 11:07:51 +0100

    added access to localStorage

--- Electron/ExpenseTracker/index.js
+++ Electron/ExpenseTracker/index.js
@@ -2,5 +2,16 @@ function InitializeMain(){
     let Header = document.getElementById("Header");
     Header.style.transform = 'scale(1)';
     Header.style.opacity = '1';
-    console.log("A")
+    UpdateBalance();
+}
+function AddExpense(){
+    let expense = document.querySelector("#ExpenseValue").value;
+    localStorage.setItem("Balance", expense);
+    UpdateBalance();
+}
+
+function UpdateBalance(){
+    let Balance = localStorage.getItem("Balance");
+    let BalanceValue = document.getElementById("Balance");
+    BalanceValue.textContent = "Total Balance: " + Balance;
 }
