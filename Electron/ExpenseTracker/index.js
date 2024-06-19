a481e9f4c0dbcb0ec2f20a466d2d11c321a00904
HugoNogueira05 <hugonogueira055@gmail.com>
2024-06-19 01:56:46 +0100

    updated

--- Electron/ExpenseTracker/index.js
+++ Electron/ExpenseTracker/index.js
@@ -2,16 +2,39 @@ function InitializeMain(){
     let Header = document.getElementById("Header");
     Header.style.transform = 'scale(1)';
     Header.style.opacity = '1';
+    if (localStorage.getItem("Balance") == null){
+        localStorage.setItem("Balance", 0);
+    }
     UpdateBalance();
+    setInterval( displayBalance , 2000);
+}
+function AddIncome(){
+    let income = document.querySelector("#IncomeValue").value;
+    if (income != ""){
+        localStorage.setItem("Balance" , parseFloat(localStorage.getItem("Balance")) + parseFloat(income));
+        UpdateBalance();
+    }
 }
 function AddExpense(){
     let expense = document.querySelector("#ExpenseValue").value;
-    localStorage.setItem("Balance", expense);
-    UpdateBalance();
+    if (expense != ""){
+        localStorage.setItem("Balance" , parseFloat(localStorage.getItem("Balance")) - parseFloat(expense));
+        UpdateBalance();
+    }
 }
 
 function UpdateBalance(){
     let Balance = localStorage.getItem("Balance");
     let BalanceValue = document.getElementById("Balance");
     BalanceValue.textContent = "Total Balance: " + Balance;
+}
+
+function ClearBalance(){
+    localStorage.setItem("Balance" ,0);
+    UpdateBalance();
+}
+
+
+function displayBalance(){
+     document.getElementById("BalanceTab").style.display = 'flex';
 }
